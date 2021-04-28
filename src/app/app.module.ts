59c921c9d9d605c9f5609d57564aab3b9218760b
Barraki <Barraki201@gmail.com>
2021-04-28 20:53:20 +0300

    Add container and card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColumnComponent } from './column/column.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColumnComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
